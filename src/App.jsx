3c9dea01845a0643f4b1ffa318d17d8782f4d05d
Syed Shahwaiz Qasim <syedshahwaiz32@gmail.com>
2025-01-04 19:17:23 +0500

    router setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
+import { BrowserRouter, Route, Routes } from 'react-router'
 import './App.css'
 import Navbar from './components/navbar'
+import Home from './pages/Home/home'
 
 function App() {
   return (
-    <>
-    <Navbar />
-    </>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
